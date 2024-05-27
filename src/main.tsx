71330b2b9c9a36479c0d72fe8b4dfff24ba1eb97
volkan <volkankanat.mdbf17@iste.edu.tr>
2024-05-27 16:41:34 +0300

    redux add and store conf.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+// Redux
+import { store } from './redux/store.ts'
+import { Provider } from 'react-redux';
+//
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
 )
