132237951ac0d2cf5d9c31ec77e3d931619dd881
volkan <volkankanat.mdbf17@iste.edu.tr>
2024-05-27 20:15:32 +0300

    fix AppDispatch type

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import './App.css'
+import  { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAllLocationsThunk } from './redux/locationSlice'; // Adjust the path as needed
+import { AppDispatch, RootState } from './redux/store'; // Adjust the path as needed
+import './App.css';
 
 function App() {
+  const dispatch = useDispatch<AppDispatch>();
+  const locations = useSelector((state: RootState) => state.locations);
+
+  useEffect(() => {
+    dispatch(fetchAllLocationsThunk());
+  }, [dispatch]);
 
   return (
     <>
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Locations</h1>
+      <ul>
+        {locations.map(location => (
+          <li key={location.id}>{location.name}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
